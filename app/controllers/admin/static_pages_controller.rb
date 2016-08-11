b546e8779f5d18d4764580b440f65ef72d7ac8cf
leduykhanhhedspi <leduykhanh.hedspi@gmail.com>
2016-08-11 19:17:16 +0700

    fix

--- app/controllers/admin/static_pages_controller.rb
+++ app/controllers/admin/static_pages_controller.rb
@@ -1,18 +1,7 @@
-class StaticPagesController < ApplicationController
-  before_action :verify_admin
-  
+class Admin::StaticPagesController < Admin::BaseController
   def home
-    @lessons = current_user.lessons.includes(:results)
-      .order(created_at: :desc).page params[:page]
-    @activities = PublicActivity::Activity.all_activity(current_user.id)
-      .order(created_at: :desc).page params[:page]
-  end
-
-  def about
-  end
-
-  private
-  def verify_admin
-    redirect_to admin_root_url if current_user.admin?
+    [User, Activity, Word, Category].each do |model|
+      instance_variable_set "@#{model.table_name}", model.all
+    end
   end
 end
